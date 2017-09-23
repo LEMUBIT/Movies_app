fca20a1a4110025b7f95c069c31ad455ba365128
lemuel <lemuelcco@gmail.com>
2017-09-23 22:34:47 +0100

    clean up code

--- app/src/main/java/com/lemubit/lemuel/popular_movies_stage1/MainActivity.java
+++ app/src/main/java/com/lemubit/lemuel/popular_movies_stage1/MainActivity.java
@@ -2,15 +2,18 @@ package com.lemubit.lemuel.popular_movies_stage1;
 
 
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    RecyclerView movieRecycler;
     static String APIkey=  BuildConfig.Movie_API;
     private static final String POPULAR_URL="https://api.themoviedb.org/3/movie/popular?page=1&language=en-US&api_key="+APIkey;
     private static final String TOP_RATED="https://api.themoviedb.org/3/movie/top_rated?page=1&language=en-US&api_key="+APIkey;
@@ -19,11 +22,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        movieRecycler = (RecyclerView) findViewById(R.id.movie_recycler_view);
         new AsyncMovie(MainActivity.this).execute(POPULAR_URL);
     }
 
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        //to make it more user friendly :)
+        if(MainActivity.this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            movieRecycler.setLayoutManager(new GridLayoutManager(MainActivity.this,2));
+        }
+        else{
+            movieRecycler.setLayoutManager(new GridLayoutManager(MainActivity.this,4));
+        }
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
